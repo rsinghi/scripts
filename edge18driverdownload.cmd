70d47fe62049f27d9da8853a6c9ea13ae9b2d080
Ruchi Singhi <rsinghi@users.noreply.github.com>
2020-04-21 11:13:20 -0700

    Reg key support

--- edge18driverdownload.cmd
+++ edge18driverdownload.cmd
@@ -1,3 +1,12 @@
 @echo off
+echo starting prerun
+echo Windows Registry Editor Version 5.00 > disablesavepw.reg
+echo. >> disablesavepw.reg
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main] >> disablesavepw.reg
+echo "FormSuggest Passwords"="no" >> disablesavepw.reg
+echo content of registry file to import:
+type disablesavepw.reg
+echo importing registry file
+reg import disablesavepw.reg
 echo downloading webdriver
 DISM.exe /Online /Add-Capability /CapabilityName:Microsoft.WebDriver~~~~0.0.1.0
