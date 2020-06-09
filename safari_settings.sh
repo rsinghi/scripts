7f311e74ccae78f8edf2a34c572e90aafb503167
Ruchi Singhi <rsinghi@users.noreply.github.com>
2020-06-09 13:54:25 -0700

    Added safari clear cookies code

--- safari_settings.sh
+++ safari_settings.sh
@@ -5,3 +5,19 @@ defaults write com.apple.Safari WarnAboutFraudulentWebsites false
 # Block pop-up windows
 defaults write com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
+
+tell application "Safari" to activate
+delay 2
+tell application "System Events" to tell process "Safari"
+    keystroke "," using command down
+    tell window 1
+        delay 3
+        click button "Privacy" of toolbar 1
+        delay 3
+        click button 2 of group 1 of group 1
+        delay 1
+        click button "Remove Now" of sheet 1
+
+    end tell
+    keystroke "w" using command down
+end tell
