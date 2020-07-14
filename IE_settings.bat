ce688a1e777855ff17614cefe1c3420033fd321c
Ruchi Singhi <rsinghi@users.noreply.github.com>
2020-07-14 16:18:44 -0700

    Update IE_settings.bat

--- IE_settings.bat
+++ IE_settings.bat
@@ -1,3 +1,4 @@
 @echo off
-reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /t REG_SZ /d "{25336920-03F9-11cf-8FD0-00AA00686F13}"; 
-reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /v Encoding /t REG_DWORD /d 0x08000000
+reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /f
+reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /v "CLSID" /t REG_SZ /d "{25336920-03F9-11cf-8FD0-00AA00686F13}" /f
+reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /v "Encoding" /t REG_DWORD /d 0x08000000 /f
