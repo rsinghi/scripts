466c6354049f4b0316c7795d54ea0b9e4288a3f4
Ruchi Singhi <rsinghi@users.noreply.github.com>
2020-07-14 16:47:50 -0700

    Update IE_settings.bat

--- IE_settings.bat
+++ IE_settings.bat
@@ -1,3 +1,8 @@
+rem Run as admin
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+rem Start the script to set the reg keys
 @echo off
 reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /f
 reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /v "CLSID" /t REG_SZ /d "{25336920-03F9-11cf-8FD0-00AA00686F13}" /f
