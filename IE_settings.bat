96a1d944ab8cd4400ddb2c88bb6a5458da292a89
Ruchi Singhi <rsinghi@users.noreply.github.com>
2020-07-20 14:42:12 -0700

    Update IE_settings.bat

--- IE_settings.bat
+++ IE_settings.bat
@@ -4,6 +4,7 @@ cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) &&
 
 rem Start the script to set the reg keys
 @echo off
+rem Set regkeys to force IE to display application/json instead of offering to download prompt
 reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /f
 reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /v "CLSID" /t REG_SZ /d "{25336920-03F9-11cf-8FD0-00AA00686F13}" /f
 reg add "HKEY_CLASSES_ROOT\MIME\Database\Content Type\application/json" /v "Encoding" /t REG_DWORD /d 0x08000000 /f
